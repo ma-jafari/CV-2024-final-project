c51f57c99fc34e67e75eafa8c1c896100c5c8e01
matteodegobbi <matteodegobbi2001@gmail.com>
2024-06-07 10:30:39 +0200

    get_strictest_line is done, now i just need to draw them for the report

--- src/field_detection.cpp
+++ src/field_detection.cpp
@@ -5,9 +5,11 @@
 #include "opencv2/core/types.hpp"
 #include <cmath>
 #include <iostream>
+#include <limits>
 #include <opencv2/core.hpp>
 #include <opencv2/highgui.hpp>
 #include <opencv2/imgproc.hpp>
+#include <strings.h>
 #include <vector>
 
 #define PRINTS
@@ -80,6 +82,25 @@ void get_line_clusters(const vector<float> &vec, vector<int> &labels) {
   kmeans(vec, n_clusters, labels, TermCriteria(TermCriteria::EPS, 10, 1.0), 3,
          KMEANS_PP_CENTERS, noArray());
 }
+
+int get_strictest_line(const vector<float> &rhos, const vector<float> &thetas,
+                       const Point image_center) {
+  float min_dist = numeric_limits<float>::max();
+  int index_closest_line = -1;
+  for (int i = 0; i < rhos.size(); ++i) {
+    float a, b, c;
+    a = cos(thetas[i]), b = sin(thetas[i]);
+    c = -rhos[i];
+    // NOTE: since ||(a,b)||=sqrt(sin(theta)^2+cos(theta)^2) = 1 for all theta
+    // I dont need to divide by this norm when computing the distance
+    float dist = fabs(a * image_center.x + b * image_center.y + c);
+    if (dist < min_dist) {
+      min_dist = dist;
+      index_closest_line = i;
+    }
+  }
+  return index_closest_line;
+}
 } // namespace
 
 void detect_field(const cv::Mat &input_image) {
@@ -103,7 +124,7 @@ void detect_field(const cv::Mat &input_image) {
   get_field_mask(graycontours, false, mask, lines);
   // TODO:
 
-  for (auto &el : lines) {
+  for (auto el : lines) {
     thetas.push_back(el[1] < CV_PI / 2 ? CV_PI - el[1] : el[1]);
   }
   constexpr int n_clusters = 2;
@@ -116,6 +137,7 @@ void detect_field(const cv::Mat &input_image) {
   ;
 
   for (size_t i = 0; i < thetas.size(); i++) {
+
     float rho = lines[i][0], theta = lines[i][1];
     Point pt1, pt2;
     double a = cos(theta), b = sin(theta);
@@ -144,12 +166,27 @@ void detect_field(const cv::Mat &input_image) {
       groupB_thetas.push_back(lines[i][1]);
     }
   }
-  cout << "A:" << groupA.size() << endl;
-  cout << "B:" << groupB.size() << endl;
+  //  cout << "A:" << groupA.size() << endl;
+  //  cout << "B:" << groupB.size() << endl;
 
   Mat color_lines = Mat::zeros(in.rows, in.cols, in.type());
   get_line_clusters(groupA, labels);
 
+  vector<float> groupC;
+  vector<float> groupC_thetas;
+  vector<float> groupD;
+  vector<float> groupD_thetas;
+  for (int i = 0; i < groupA.size(); ++i) {
+    cout << labels[i] << endl;
+    if (labels[i] == 0) {
+      groupC.push_back(groupA[i]);
+      groupC_thetas.push_back(groupA_thetas[i]);
+    } else {
+      groupD.push_back(groupA[i]);
+      groupD_thetas.push_back(groupA_thetas[i]);
+    }
+  }
+  cout << "size group C" << groupC_thetas.size() << endl;
 #ifdef PRINTS
   for (size_t i = 0; i < groupA.size(); i++) {
     float rho = groupA[i], theta = groupA_thetas[i];
@@ -160,10 +197,48 @@ void detect_field(const cv::Mat &input_image) {
     pt1.y = cvRound(y0 + 1000 * (a));
     pt2.x = cvRound(x0 - 1000 * (-b));
     pt2.y = cvRound(y0 - 1000 * (a));
+    cout << labels[i] << endl;
     line(color_lines, pt1, pt2, colori[2 + labels[i]], 3, LINE_AA);
   }
 #endif
 
+  const Point image_center(color_lines.cols / 2, color_lines.rows / 2);
+  circle(color_lines, image_center, 10, Scalar(255, 0, 255));
+
+  int index_closest_line =
+      get_strictest_line(groupC, groupC_thetas, image_center);
+  float rho = groupC[index_closest_line],
+        theta = groupC_thetas[index_closest_line];
+  Point pt1, pt2;
+  double a = cos(theta), b = sin(theta);
+  cout << "a " << a << endl;
+  cout << "b " << b << endl;
+  cout << "rho " << rho << endl;
+  cout << "---------------------------------" << endl;
+  double x0 = a * rho, y0 = b * rho;
+  pt1.x = cvRound(x0 + 1000 * (-b));
+  pt1.y = cvRound(y0 + 1000 * (a));
+  pt2.x = cvRound(x0 - 1000 * (-b));
+  pt2.y = cvRound(y0 - 1000 * (a));
+  line(color_lines, pt1, pt2, Scalar(255, 0, 255), 3, LINE_AA);
+
+  ////////////////////////
+
+  index_closest_line = get_strictest_line(groupD, groupD_thetas, image_center);
+  rho = groupD[index_closest_line], theta = groupD_thetas[index_closest_line];
+  pt1, pt2;
+  a = cos(theta), b = sin(theta);
+  cout << "a " << a << endl;
+  cout << "b " << b << endl;
+  cout << "rho " << rho << endl;
+  cout << "---------------------------------" << endl;
+  x0 = a * rho, y0 = b * rho;
+  pt1.x = cvRound(x0 + 1000 * (-b));
+  pt1.y = cvRound(y0 + 1000 * (a));
+  pt2.x = cvRound(x0 - 1000 * (-b));
+  pt2.y = cvRound(y0 - 1000 * (a));
+  line(color_lines, pt1, pt2, Scalar(255, 0, 255), 3, LINE_AA);
+  ////HROUP B
   get_line_clusters(groupB, labels);
 
 #ifdef PRINTS
@@ -179,44 +254,11 @@ void detect_field(const cv::Mat &input_image) {
     line(color_lines, pt1, pt2, colori[labels[i]], 3, LINE_AA);
   }
 #endif
-  Point center_of_image(in.cols / 2, in.rows / 2);
 
-  vector<float> groupC;
-  vector<float> groupC_thetas;
-  vector<float> groupD;
-  vector<float> groupD_thetas;
-  for (int i = 0; i < groupA.size(); ++i) {
-    if (labels[i] == 0) {
-      groupC.push_back(groupA[i]);
-      groupC_thetas.push_back(groupA_thetas[i]);
-    } else {
-      groupD.push_back(groupA[i]);
-      groupD_thetas.push_back(groupA_thetas[i]);
-    }
-  }
-  float min_dist = INFINITY;
-  int index_closest_line = -1;
-  for (int i = 0; i < groupC.size(); ++i) {
-    float dist = fabs(center_of_image.x * cos(groupC_thetas[i]) +
-                      center_of_image.y * sin(groupC_thetas[i])) -
-                 groupC[i];
-    if (dist < min_dist) {
-      min_dist = dist;
-      index_closest_line = i;
-    }
-  }
+  cout << "########################################" << endl;
 
-  cout << index_closest_line << endl;
-  float rho = groupC[index_closest_line],
-        theta = groupC_thetas[index_closest_line];
-  Point pt1, pt2;
-  double a = cos(theta), b = sin(theta);
-  double x0 = a * rho, y0 = b * rho;
-  pt1.x = cvRound(x0 + 1000 * (-b));
-  pt1.y = cvRound(y0 + 1000 * (a));
-  pt2.x = cvRound(x0 - 1000 * (-b));
-  pt2.y = cvRound(y0 - 1000 * (a));
-  line(color_lines, pt1, pt2, Scalar(255, 255, 255), 3, LINE_AA);
+#ifdef PRINTS
+#endif
   imshow("cc", color_lines);
   waitKey(0);
 }
